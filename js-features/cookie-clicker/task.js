0c4085efeb0c3e57025dcac4cb4acf6b168472cf
Alexander Boriskin <iboriskin5@gmail.com>
2024-04-09 17:10:19 +0300

    cookie-clicker extra completed

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,21 @@
 const clickerCounter = document.getElementById('clicker__counter');
+const clickerSpeed = document.getElementById('clicker__speed');
 const img = document.getElementById('cookie');
 
 var counter = Number(clickerCounter.textContent);
 
+let clickTimestamp = new Date().getTime();
+
 img.onclick = () => {
     counter++;
+
+    newClickTimestamp = new Date().getTime();
+    speed = 1000 / (newClickTimestamp - clickTimestamp);
+    clickTimestamp = newClickTimestamp;
+
     clickerCounter.textContent = counter;
-    
+    clickerSpeed.textContent = Math.round(speed * 100) / 100;
+
     if (counter % 2) {
         img.width += 20;
         img.height += 15;
@@ -15,11 +24,3 @@ img.onclick = () => {
         img.height -= 15;
     }
 }
-
-clicksBefore = 0;
-
-setInterval(() => {
-    clicksAfter = counter;
-    console.log(clicksAfter - clicksBefore);
-    clicksBefore = counter;
-}, 1000)
