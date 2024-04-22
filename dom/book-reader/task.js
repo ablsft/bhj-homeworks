dde5333543ad23ea24e08c4bf8dcac59c8e4f5e5
Alexander Boriskin <iboriskin5@gmail.com>
2024-04-22 13:49:37 +0300

    book-reader extra completed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,23 +1,77 @@
-const bookControl = document.querySelector('.book__control');
-const bookContent = document.querySelector('.book__content')
+const bookControlFontSize = document.querySelector('.book__control_font-size');
+const bookControlColor = document.querySelector('.book__control_color');
+const bookControlBackground = document.querySelector('.book__control_background');
+const bookContent = document.querySelector('.book__content');
+
+let fontSizeLastModified = null;
+let textColorLastModified = null;
+let backgroundColorLastModified = null;
 
 function chooseFontSize(event) {
     let fontSizeActive = document.querySelector('.font-size_active')
     let target = event.target;
 
     if (target.classList.contains('font-size')) {
-        bookContent.className = 'book';
-
+        if (fontSizeLastModified) {
+            bookContent.classList.remove(fontSizeLastModified);
+        }
+        
         fontSizeActive.classList.remove('font-size_active');
         target.classList.add('font-size_active');
 
         let dataSize = target.getAttribute('data-size');
         if (dataSize) {
-            bookContent.classList.add(`book_fs-${dataSize}`)
+            let fontSizeClass = `book_fs-${dataSize}`;
+            bookContent.classList.add(fontSizeClass);
+            fontSizeLastModified = fontSizeClass;
         }
     }
 
     event.preventDefault();
 }
 
-bookControl.addEventListener('click', chooseFontSize);
+function chooseTextColor(event) {
+    let textColorActive = bookControlColor.querySelector('.color_active');
+    let target = event.target;
+
+    if (target.classList.contains('color')) {
+        if (textColorLastModified) {
+            bookContent.classList.remove(textColorLastModified);
+        }
+
+        textColorActive.classList.remove('color_active');
+        target.classList.add('color_active');
+
+        let dataTextColor = target.getAttribute('data-text-color');
+        let dataTextColorClass = `book_color-${dataTextColor}`;
+        bookContent.classList.add(dataTextColorClass);
+        textColorLastModified = dataTextColorClass;
+    }
+
+    event.preventDefault();
+}
+
+function chooseBackgroundColor(event) {
+    let backgroundColorActive = bookControlBackground.querySelector('.color_active');
+    let target = event.target;
+
+    if (target.classList.contains('color')) {
+        if (backgroundColorLastModified) {
+            bookContent.classList.remove(backgroundColorLastModified);
+        }
+
+        backgroundColorActive.classList.remove('color_active');
+        target.classList.add('color_active');
+
+        let dataBackgroundColor = target.getAttribute('data-bg-color');
+        let dataBackgroundColorClass = `book_bg-${dataBackgroundColor}`;
+        bookContent.classList.add(dataBackgroundColorClass);
+        backgroundColorLastModified = dataBackgroundColorClass;
+    }
+
+    event.preventDefault();
+}
+
+bookControlFontSize.addEventListener('click', chooseFontSize);
+bookControlColor.addEventListener('click', chooseTextColor);
+bookControlBackground.addEventListener('click', chooseBackgroundColor);
