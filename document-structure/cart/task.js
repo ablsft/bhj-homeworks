6a3c76aafdd4c8d6c2366be4f42d7541d3fd402f
Alexander Boriskin <iboriskin5@gmail.com>
2024-05-01 01:21:09 +0300

    cart extra 1 completed

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,5 +1,4 @@
 const products = document.querySelectorAll('.product')
-const cartProducts = document.querySelector('.cart__products');
 
 
 products.forEach(function (product) {
@@ -15,9 +14,25 @@ products.forEach(function (product) {
         } else if (e.target.classList.contains('product__quantity-control_inc')) {
             productQuantityValue.textContent = productQuantityValueNumber + 1;
 
-        } else if (e.target.classList.contains('product__add')) {
+        } else {
+
+            if (!document.querySelector('.cart')) {
+                const cart = document.createElement('div');
+                cart.classList.add('cart');
+
+                const cartTitle = document.createElement('div');
+                cartTitle.classList.add('cart__title');
+                cartTitle.innerText = 'Корзина';
+
+                const cartProductsElement = document.createElement('div');
+                cartProductsElement.classList.add('cart__products');
+
+                cart.append(cartTitle, cartProductsElement);
+                document.body.insertBefore(cart, document.querySelector('.products'));
+            }
+
+            const cartProducts = document.querySelector('.cart__products');
             const productId = product.dataset.id;
-            const productImage = product.querySelector('img').getAttribute('src');
 
             let cartProduct = null;
             for (const productInCart of cartProducts.children) {
@@ -27,25 +42,51 @@ products.forEach(function (product) {
                 }
             }
 
-            if (cartProduct) {
-                let cartProductCount = cartProduct.lastChild;
-                cartProductCount.textContent = Number(cartProductCount.textContent) + Number(productQuantityValue.textContent);
-            
-            } else {
-                cartProduct = document.createElement('div');
-                cartProduct.classList.add('cart__product');
-                cartProduct.dataset.id = productId;
+            if (e.target.classList.contains('product__add')) {
+                if (!product.querySelector('.product__delete')) {
+                    const productDelete = document.createElement('div');
+                    productDelete.classList.add('product__delete');
+                    productDelete.innerText = 'Удалить из корзины';
+    
+                    const productQuantity = product.querySelector('.product__quantity');
+                    productQuantity.append(productDelete);
+                }
                 
-                const cartProductImage = document.createElement('img');
-                cartProductImage.setAttribute('src', productImage);
-                cartProductImage.classList.add('cart__product-image');
-                
-                const cartProductCount = document.createElement('div');
-                cartProductCount.classList.add('cart__product-count');
-                cartProductCount.innerText = productQuantityValue.textContent;
+                const productImage = product.querySelector('img').getAttribute('src');
 
-                cartProducts.append(cartProduct);
-                cartProducts.lastChild.append(cartProductImage, cartProductCount);
+                if (cartProduct) {
+                    let cartProductCount = cartProduct.lastChild;
+                    cartProductCount.textContent = Number(cartProductCount.textContent) + Number(productQuantityValue.textContent);
+                
+                } else {
+                    const cartProduct = document.createElement('div');
+                    cartProduct.classList.add('cart__product');
+                    cartProduct.dataset.id = productId;
+                    
+                    const cartProductImage = document.createElement('img');
+                    cartProductImage.setAttribute('src', productImage);
+                    cartProductImage.classList.add('cart__product-image');
+                    
+                    const cartProductCount = document.createElement('div');
+                    cartProductCount.classList.add('cart__product-count');
+                    cartProductCount.innerText = productQuantityValue.textContent;
+    
+                    cartProduct.append(cartProductImage, cartProductCount);
+                    cartProducts.append(cartProduct);
+                }
+
+            } else if (e.target.classList.contains('product__delete')) {
+                let cartProductCount = cartProduct.lastChild;
+                cartProductCount.textContent = Number(cartProductCount.textContent) - Number(productQuantityValue.textContent);
+                
+                if (Number(cartProductCount.textContent) < 1) {
+                    cartProduct.remove();
+                    e.target.remove();
+                }
+
+                if (!document.querySelector('.cart__product')) {
+                    document.querySelector('.cart').remove();
+                }
             }
         }
     });
