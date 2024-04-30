eca2a912875672e9658bb8fe0af07edd0c7a3a4b
Alexander Boriskin <iboriskin5@gmail.com>
2024-04-30 12:54:03 +0300

    tooltip extra #1 completed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -8,7 +8,15 @@ hasTooltips.forEach(function (hasTooltip) {
     hasTooltip.insertAdjacentElement('afterend', tooltip);
 
     hasTooltip.addEventListener('click', function (e) {
+        const tooltips = document.querySelectorAll('.tooltip');
         let activeTooltip = hasTooltip.nextSibling;
+        
+        tooltips.forEach(function (tooltip) {
+            if (tooltip != activeTooltip) {
+                tooltip.classList.remove('tooltip_active');
+            }
+        })
+
         activeTooltip.classList.toggle('tooltip_active');
 
         e.preventDefault();
