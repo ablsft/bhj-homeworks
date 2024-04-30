7086c32e4d315c81fa82f9e46143604940753376
Alexander Boriskin <iboriskin5@gmail.com>
2024-04-30 20:07:28 +0300

    todo extra completed

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,6 +1,24 @@
 const tasksAdd = document.querySelector('.tasks__add');
 const tasksList = document.querySelector('.tasks__list');
 
+let taskCounter = 0;
+if (localStorage.getItem('taskCounterValue')) {
+    taskCounter = Number(localStorage.getItem('taskCounterValue'));
+}
+
+for (let i = 0; i < taskCounter; i++) {
+    if (localStorage.getItem(i)) {
+        tasksList.insertAdjacentHTML('beforeend', localStorage.getItem(i));
+
+        let lastTask = tasksList.lastChild;
+        
+        lastTask.lastChild.addEventListener('click', () => {
+            lastTask.remove();
+            localStorage.removeItem(i);
+        });
+    }
+}
+
 tasksAdd.addEventListener('click', (e) => {
     const taskInput = document.getElementById('task__input');
     
@@ -18,14 +36,19 @@ tasksAdd.addEventListener('click', (e) => {
         a.classList.add('task__remove');
         a.innerText = '×';
 
+        task.append(taskTitle);
+        task.append(a);
         tasksList.append(task);
 
-        const taskDom = tasksList.lastChild;
-        taskDom.append(taskTitle);
-        taskDom.append(a);
+        localStorage.setItem(taskCounter, task.outerHTML);
+        taskCounter += 1;
+        localStorage.setItem('taskCounterValue', taskCounter);
 
-        taskDom.lastChild.addEventListener('click', (e) => {
-            taskDom.remove();
-        })
+        task.lastChild.addEventListener('click', () => {
+            task.remove();
+            localStorage.removeItem(taskCounter);
+        });
     }
-})
+
+    e.preventDefault();
+});
