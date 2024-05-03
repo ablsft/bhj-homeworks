c9f6036b521df35bc7b867474dfbe239b6d2256d
Alexander Boriskin <iboriskin5@gmail.com>
2024-05-03 06:19:48 +0300

    initial commit progressbar

--- async-requests/progressbar/main.js
+++ async-requests/progressbar/main.js
@@ -1,6 +1,24 @@
+const form = document.forms.form;
+const progress = document.getElementById('progress');
+
 document.getElementById("file").onchange = function() {
+  progress.value = 0;
   const fileDesc = document.querySelector(".input__wrapper-desc");
   let fileName = this.value.split("\\");
   fileName = fileName[fileName.length - 1];
   fileDesc.textContent = fileName;
 };
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const sendFormRequest = new XMLHttpRequest();
+
+  sendFormRequest.upload.onprogress = function(e) {
+    progress.value = e.loaded / e.total;
+  }
+
+  sendFormRequest.open('POST', form.action);
+  const formData = new FormData(form);
+  sendFormRequest.send(formData);
+})
