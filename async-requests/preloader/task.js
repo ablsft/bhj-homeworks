b94d1121028e1f36ecb9c4c0ed96641128bb8873
Alexander Boriskin <iboriskin5@gmail.com>
2024-05-07 13:29:46 +0300

    preloader extra completed

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -3,31 +3,49 @@ const courses = new XMLHttpRequest();
 const loader = document.getElementById('loader');
 const items = document.getElementById('items');
 
+valutesArray = [];
+
+function addValuteToDom(itemCode, itemValue, parent) {
+    parent.insertAdjacentHTML('beforeend', `
+        <div class="item">
+            <div class="item__code">${itemCode}</div>
+            <div class="item__value">${itemValue}</div>
+            <div class="item__currency">руб.</div>
+        </div>
+    `);
+}
+
+if (localStorage.getItem('valutesArray')) {
+    valutesArray = JSON.parse(localStorage.getItem('valutesArray'));
+
+    valutesArray.forEach((valuteElement) => {
+        addValuteToDom(valuteElement[0], valuteElement[1], items);
+    });
+}
+
 courses.addEventListener('readystatechange', () => {
     if (courses.readyState === courses.DONE) {
         loader.classList.remove('loader_active');
+        localStorage.removeItem('valutesArray');
+        valutesArray = [];
 
+        items.remove();
+        itemsEmpty = document.createElement('div');
+        itemsEmpty.classList.add('items');
+        document.querySelector('.card').append(itemsEmpty);
+        
         const coursesParsed = JSON.parse(courses.response);
         const valutes = coursesParsed.response.Valute;
 
         for (const valute in valutes) {
-            const item = document.createElement('div');
-            item.classList.add('item');
+            addValuteToDom(valutes[valute].CharCode, valutes[valute].Value, itemsEmpty);
+        
+            if (localStorage.getItem('valutesArray')) {
+                valutesArray = JSON.parse(localStorage.getItem('valutesArray'));
+            }
 
-            const itemCode = document.createElement('div');
-            itemCode.classList.add('item__code');
-            itemCode.innerText = valutes[valute].CharCode;
-
-            const itemValue = document.createElement('div');
-            itemValue.classList.add('item__value');
-            itemValue.innerText = valutes[valute].Value;
-
-            const itemCurrency = document.createElement('div');
-            itemCurrency.classList.add('item__currency');
-            itemCurrency.innerText = 'руб.'
-
-            item.append(itemCode, itemValue, itemCurrency);
-            items.append(item);
+            valutesArray.push([valutes[valute].CharCode, valutes[valute].Value]);
+            localStorage.setItem('valutesArray', JSON.stringify(valutesArray));
         }
     }
 })
